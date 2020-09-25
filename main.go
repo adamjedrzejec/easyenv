b7ae9ada165acf8d01a4b57fed0f516a043c3001
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-09-26 01:11:38 +0200

    [add] function getting environment variables from .env file and converts it to slices

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	env "github.com/Netflix/go-env"
 )
@@ -11,6 +15,47 @@ type environmentStruct struct {
 	Home string `env:"HOME"`
 
 	Extras env.EnvSet // not explicitly invoked environment variables go there
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Errors returned
+var (
+	ErrBadFormat = errors.New("Incorrect format of environment variables")
+)
+
+// GetEnv returns the map filled with env vars from the .env file
+func GetEnv() (map[string]string, error) {
+	file, err := os.Open("./.env")
+	check(err)
+	defer file.Close()
+
+	var variables map[string]string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		varSlice := strings.FieldsFunc(scanner.Text(), func(c rune) bool {
+			if c == '=' || c == ' ' {
+				return true
+			}
+			return false
+		})
+
+		if len(varSlice) != 2 {
+			return nil, ErrBadFormat
+		}
+
+		fmt.Println(varSlice)
+	}
+
+	// checks if Scan() returned false because of EOF
+	check(scanner.Err())
+
+	return variables, nil
 }
 
 func main() {
@@ -27,7 +72,8 @@ func main() {
 	fmt.Println("Home =", environment.Home)
 
 	fmt.Println("Number of extra environment variables:", len(environment.Extras))
-	for key, val := range environment.Extras {
-		fmt.Printf("%s=%s\n", key, val)
-	}
+
+	_, err = GetEnv()
+
+	check(err)
 }
